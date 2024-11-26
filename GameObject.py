ff2843739083641313670687d5d0e57319aeed7b
Jim Stewart <jim.stewart@thelangschool.org>
2024-11-26 13:05:18 -0500

    changed actions in GameObject to dictionary

--- GameObject.py
+++ GameObject.py
@@ -1,9 +1,9 @@
 class GameObject:
-    def __init__(self, desc, inside = None, flags = None, actions = None, synonyms = None, capacity = 0):
+    def __init__(self, desc, inside = None, flags = None, synonyms = None, capacity = 0):
         self.desc = desc
         self.flags = flags
         self.inside = inside
-        self.actions = actions
+        self.actions = {}
         self.synonyms = synonyms
         self.capacity = capacity
 
--- Room.py
+++ Room.py
@@ -1,7 +1,7 @@
 from GameObject import GameObject
 
 class Room(GameObject):
-    def __init__(self, desc, inside = None, flags = None, actions = None, synonyms = None, capacity = 0):
-        super().__init__(desc, inside, flags, actions, synonyms, capacity)
+    def __init__(self, desc, inside = None, flags = None, synonyms = None, capacity = 0):
+        super().__init__(desc, inside, flags, synonyms, capacity)
         self.directions = {"north":None, "South":None, "East":None, "West":None, "Up": None, "Down":None}
         
